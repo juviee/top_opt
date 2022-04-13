bc20848aeb1994dcb5e929325d2046cd8871ca9b
b_umutbaev <b_umutbaev@protonmail.com>
2022-04-14 01:12:48 +0300

    implemented settings creator from .ini file

--- read_settings.m
+++ read_settings.m
@@ -1,6 +1,24 @@
-function read_settings(s_filename):
+function read_settings(s_filename)
     ini_st = ini2struct(s_filename);
     s = settings;
-    addGroup(s, 'topoptSettings');
-    addSetting(s.topoptSettings, '')
+    try
+        removeGroup(s, 'topOptSettings');
+    catch
+    end
+    addGroup(s, "topOptSettings");
+    fn_lvl1 = fieldnames(ini_st);
+    for i=1:numel(fn_lvl1)
+        lvl1_name_tmp = string(fn_lvl1(i));
+        lvl1_field_tmp = ini_st.(string(lvl1_name_tmp));
+        fn_lvl2 = fieldnames(lvl1_field_tmp);
+        addGroup(s.topOptSettings, lvl1_name_tmp);
+        for i=1:numel(fn_lvl2)
+            lvl2_name_tmp = string(fn_lvl2(i));
+            lvl2_field_tmp = lvl1_field_tmp.(string(lvl2_name_tmp));
+            addSetting(s.topOptSettings.(lvl1_name_tmp),lvl2_name_tmp);
+            s.topOptSettings.(lvl1_name_tmp).(lvl2_name_tmp).PersonalValue = lvl2_field_tmp;
+            fprintf("%s.%s.%s\n",lvl1_name_tmp, lvl2_name_tmp, lvl2_field_tmp);
+            fprintf('%s.\n', s.topOptSettings.(lvl1_name_tmp).(lvl2_name_tmp).ActiveValue)
+        end
+    end
 end
